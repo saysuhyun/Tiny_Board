4b35a25959972567dded1abddf13f2639d2ab658
saysuhyun <saysuhyun@gmail.com>
2025-04-12 09:14:15 +0900

    comment

--- back/src/main/java/com/example/back/controller/UserController.java
+++ back/src/main/java/com/example/back/controller/UserController.java
@@ -16,12 +16,14 @@ public class UserController {
         this.userService = userService;
     }
 
+    // 유저 경로에 Post인 경우는 회원가입
     @PostMapping("")
     public ResponseEntity<User> createUser(@RequestParam String username, @RequestParam String password, @RequestParam String email) {
         User user = userService.createUser(username, password, email);
         return ResponseEntity.ok(user);
     }
 
+    // 유저 경로 id 넣어서 delete인 경우는 탈퇴
     @DeleteMapping("{userId}")
     public ResponseEntity<Void> deleteUser(@Parameter(description = "ID of the user to be deleted", required = true) @PathVariable Long userId) {
         userService.deleteUser(userId);
